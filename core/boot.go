b6da59fa5fb034012b34c7aa98af2f874b5e72f2
Nowfel Mashnoor <nmmashnoor@gmail.com>
2022-03-27 19:16:55 +0600

    Added successful query in ES

--- core/boot.go
+++ core/boot.go
@@ -1,8 +1,13 @@
 package core
 
 import (
-	"github.com/elastic/go-elasticsearch/v8"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/mashnoor/pigeon/settings"
+	"log"
 	"sync"
 )
 
@@ -21,7 +26,7 @@ func initNotificationSystem() {
 }
 
 func BootApp() {
-	settings.LoadAppConfig()
+	//settings.LoadAppConfig()
 	//initNotificationSystem()
 	cfg := elasticsearch.Config{
 		Addresses: []string{
@@ -29,5 +34,76 @@ func BootApp() {
 		},
 	}
 	es, err := elasticsearch.NewClient(cfg)
+
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"must": []map[string]interface{}{
+					{
+						"match": map[string]interface{}{
+							"kubernetes.labels.app": "ucbpg",
+						},
+					},
+					{
+						"match_phrase": map[string]string{
+							"log": "Money has been successfully added",
+						},
+					},
+				},
+
+				"filter": map[string]interface{}{
+					"range": map[string]interface{}{
+						"@timestamp": map[string]string{
+							"gte": "2022-03-27T00:55:47.165Z",
+						},
+					},
+				},
+			},
+		},
+	}
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Fatalf("Error encoding query: %s", err)
+	}
+
+	fmt.Println(buf.String())
+
+	var r map[string]interface{}
+	res, err := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("logstash-*"),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			log.Fatalf("Error parsing the response body: %s", err)
+		} else {
+			// Print the response status and error information.
+			log.Fatalf("[%s] %s: %s",
+				res.Status(),
+				e["error"].(map[string]interface{})["type"],
+				e["error"].(map[string]interface{})["reason"],
+			)
+		}
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+	// Print the response status, number of results, and request duration.
+	log.Printf(
+		"[%s] %d hits; took: %dms",
+		res.Status(),
+		int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
+		int(r["took"].(float64)),
+	)
 
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,6 @@ module github.com/mashnoor/pigeon
 
 go 1.17
 
-require (
-	github.com/elastic/elastic-transport-go/v8 v8.1.0 // indirect
-	github.com/elastic/go-elasticsearch/v8 v8.1.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
+require gopkg.in/yaml.v2 v2.4.0
+
+require github.com/elastic/go-elasticsearch/v7 v7.17.1 // indirect
